35fceea96b9ba81fb6bbe34168113678b201380f
Ethem <ethemyer07@gmail.com>
2025-05-31 15:54:00 +0300

    Initial Commit

---
+++ DenizKiziCafe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DenizKiziCafe
+//
+//  Created by Ethem Yer on 31.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DenizKiziCafe/DenizKiziCafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DenizKiziCafeApp.swift
+//  DenizKiziCafe
+//
+//  Created by Ethem Yer on 31.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct DenizKiziCafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
