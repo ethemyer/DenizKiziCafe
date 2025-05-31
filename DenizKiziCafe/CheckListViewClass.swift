30d1ef140006b064e346a4dc233c1344f03f7649
Ethem <ethemyer07@gmail.com>
2025-05-31 17:13:49 +0300

    3 menu add

---
+++ DenizKiziCafe/CheckListViewClass.swift
@@ -0,0 +1,8 @@
+//
+//  CheckListViewClass.swift
+//  DenizKiziCafe
+//
+//  Created by Ethem Yer on 31.05.2025.
+//
+
+import Foundation
---
+++ DenizKiziCafe/MenuCafe.swift
@@ -0,0 +1,18 @@
+//
+//  MenuCafe.swift
+//  DenizKiziCafe
+//
+//  Created by Ethem Yer on 31.05.2025.
+//
+
+import SwiftUI
+
+struct MenuCafe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuCafe()
+}
